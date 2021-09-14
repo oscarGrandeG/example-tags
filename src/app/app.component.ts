5e1f5355050a909018eba531574132b57f2cd8f3
Oscar Grande <oscar_obgg@hotmail.com>
2021-09-13 19:36:09 -0500

    fix: tags implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'seo-app';
+export class AppComponent implements OnInit {
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit')
+    console.log(':::::ngOnInit:::::');
+    this.titleService.setTitle('Tu Depa - Home');
+    this.meta.addTag({
+      name: 'angular.ganatan',
+      content: 'danny ganatan'
+    });
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'Cette application a été développée avec Angular version 11.2.10 et bootstrap 4.6.0' +
+          ' Elle applique le Routing, le Lazy loading, le Server side rendering et les Progressive Web App (PWA)'
+      });
+
+  }
 }
